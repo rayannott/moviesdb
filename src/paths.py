58575b541e88125e2b752003a1f16f0bc2805119
Airat <airat.valiullin@asmpt.com>
2025-05-12 09:48:11 +0200

    autocreate the sql examples dir

--- src/paths.py
+++ src/paths.py
@@ -1,6 +1,7 @@
 import pathlib
 
 SQL_QUERY_EXAMPLES_DIR = pathlib.Path("query-examples-local")
+SQL_QUERY_EXAMPLES_DIR.mkdir(exist_ok=True)
 
 LOCAL_DIR = pathlib.Path("local")
 ALLOWED_USERS = LOCAL_DIR / "allowed_users.json"
