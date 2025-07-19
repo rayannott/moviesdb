cdcaba71489f40bbc75428e3dd570b586b820cdb
AiratLaptop <airat.valiullin@tum.de>
2025-07-19 14:09:24 +0200

    minor: remove entries from the signature test in books app

--- tests/test_cmd_methods_books.py
+++ tests/test_cmd_methods_books.py
@@ -27,11 +27,10 @@ SIGNATURE = inspect.Signature(
 
 def test_cmd_signatures():
     _mock_console = Console()
-    _mock_entries = []
 
     def _mock_input(x: str) -> str:
         return x
 
-    app = BooksMode(_mock_entries, _mock_console, _mock_input)
+    app = BooksMode(_mock_console, _mock_input)
     for cmd_method in app.command_methods.values():
         assert inspect.signature(cmd_method) == SIGNATURE
