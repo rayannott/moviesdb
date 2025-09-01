769d8ef1fd458808feaae4510575bccf89b9ac0a
Airat Valiullin <airat.valiullin@entrixenergy.com>
2025-09-01 09:55:51 +0200

    skip check for duplicates

--- tests/test_images.py
+++ tests/test_images.py
@@ -12,7 +12,9 @@ def test_image_store_noentries(image_store_noentries: ImagesStore):
     assert image_store_noentries.entries == []
 
 
+@pytest.mark.skip(reason="Will not check in testing: not a codebase issue")
 def test_detect_duplicates(image_store_noentries: ImagesStore):
+    # TODO: move this functionality to the app (e.g. check on startup)
     hash_groups = image_store_noentries._group_by_etag_hash()
     for hash_val, s3_ids in hash_groups.items():
         assert len(s3_ids) == 1, f"Duplicate images found for hash {hash_val}: {s3_ids}"
