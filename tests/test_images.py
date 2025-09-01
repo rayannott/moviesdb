af2129c358aa2fc9537694eb0dc5c40129c7c161
Airat <airat.valiullin@tum.de>
2025-09-01 22:51:05 +0200

    refactor: rename images_manager to image and ImagesStore to ImageManager

--- tests/test_images.py
+++ tests/test_images.py
@@ -1,19 +1,19 @@
 import pytest
 
-from src.obj.images_manager import ImagesStore
+from src.obj.image import ImageManager
 
 
 @pytest.fixture(scope="module")
 def image_store_noentries():
-    return ImagesStore([])
+    return ImageManager([])
 
 
-def test_image_store_noentries(image_store_noentries: ImagesStore):
+def test_image_store_noentries(image_store_noentries: ImageManager):
     assert image_store_noentries.entries == []
 
 
 @pytest.mark.skip(reason="Will not check in testing: not a codebase issue")
-def test_detect_duplicates(image_store_noentries: ImagesStore):
+def test_detect_duplicates(image_store_noentries: ImageManager):
     # TODO: move this functionality to the app (e.g. check on startup)
     hash_groups = image_store_noentries._group_by_etag_hash()
     for hash_val, s3_ids in hash_groups.items():
